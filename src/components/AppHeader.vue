93fea7ef5ccb6bc6c47844a91a3a102a69fe6a5d
haha <123@.com>
2018-08-07 11:21:41 +0800

    完成公共部分

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,17 @@
 <template>
-
+     <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">CRUD</a>
+        </div>
+      </div>
+    </nav>
 
 
 
@@ -7,7 +19,13 @@
 </template>
 
 <script>
-    
+    export default {
+        data() {
+            return {
+                
+            };
+        }
+    };
 
 
 </script>
